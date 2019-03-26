9bf5e0241aa7191b794c85b26e9d93fa5e85e297
Jose <jortega@digitalbakers.com>
2019-03-26 12:47:45 +0100

    Area de usuario | formulario observaciones

--- web/themes/custom/asterics/src/js/functions.js
+++ web/themes/custom/asterics/src/js/functions.js
@@ -1,7 +1,9 @@
 ( function ($, Drupal, drupalSettings) {
+
     $(document).ready(function() {
 
       userBox();
+      followControl();
 
         $('header .right .top .ico-menu').click(function() {
           $(this).toggleClass("active");
@@ -51,6 +53,58 @@
 
     });
 
+    function followControl() {
+
+      const followMessage = $('<div class="follow_message"><div class="box"><span class="close"></span>Now you follow this observatory</div></div>').hide();
+      const unFollowMessage = $('<div class="follow_message"><div class="box"><span class="close"></span>You have stopped following this observatory</div></div>').hide();
+
+      unFollowMessage.add(followMessage).find('.close').click(function(){
+        $(this).closest('.follow_message').fadeOut().remove();
+        $('body').removeClass('no_scroll');
+      })
+
+      followButtonControl($('.follow a, .follow span'));
+      unFollowButtonControl($('.unfollow a, .unfollow span'));
+
+      function followButtonControl(elem){
+        elem.each(function(){
+          $(this).click(function(e){
+            e.preventDefault();
+            $('body').append(followMessage.clone(true));
+            $('.follow_message').fadeIn();
+            $('body').addClass('no_scroll');
+            $(this).unbind('click');
+            if (reverseText($(this)))
+              unFollowButtonControl($(this));
+          })
+        })
+      }
+
+      function unFollowButtonControl(elem){
+        elem.each(function(){
+          $(this).click(function(e){
+            e.preventDefault();
+            $('body').append(unFollowMessage.clone(true));
+            $('.follow_message').fadeIn();
+            $('body').addClass('no_scroll');
+            $(this).unbind('click');
+            if (reverseText($(this)))
+              followButtonControl($(this));
+          })
+        })
+      }
+
+      function reverseText(elem){
+        if (elem.hasClass('reverse')){
+          var actualReverseText = elem.text();
+          elem.html(elem.attr('data-reverse'));
+          elem.attr('data-reverse', actualReverseText);
+          return true;
+        }
+        return false;
+      }
+    }
+
     function userBox(){
       if ($('.user_box span').length){
         $('.user_box span').click(function(e){
@@ -62,11 +116,11 @@
 
     /** PASWORD RESET BEHAVIOUR **/
     Drupal.behaviors.PasswordReset = {
-        attach : function (context,settings) {
-            if(settings.passreset) {
-                $('.user-pass-reset .form-submit').trigger('click');
-            }
+      attach : function (context,settings) {
+        if(settings.passreset) {
+          $('.user-pass-reset .form-submit').trigger('click');
         }
+      }
     }
     /** PASWORD RESET BEHAVIOUR **/
 
