9b303eae5c3a61175179874429e8ba4a79a88db8
Oriol Almirall <oalmirall@digitalbakers.com>
2019-04-16 17:58:51 +0200

    OA Suscripcio per Ajax

---
+++ web/modules/custom/demoasterics/src/Controller/SuscribeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\demoasterics\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\taxonomy\TermInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class SuscribeController extends ControllerBase {
+    
+    public function suscribe(TermInterface $taxonomy_term) {
+        
+        $taxonomy_term->field_observatory_suscribers[] = \Drupal::currentUser()->id();
+        $taxonomy_term->save();
+        return new JsonResponse(array('message'=>"You have been suscribed to ".$taxonomy_term->name->value));
+    }   
+    public function unsuscribe(TermInterface $taxonomy_term) {
+        
+        $suscribers = $taxonomy_term->get('field_observatory_suscribers')->getValue();
+        $key = array_search(\Drupal::currentUser()->id(), array_column($suscribers, 'target_id'));
+        $taxonomy_term->get('field_observatory_suscribers')->removeItem($key);
+        $taxonomy_term->save();
+        
+        return new JsonResponse(array('message'=>"You have been unsuscribed to ".$taxonomy_term->name->value));
+    }
+}
